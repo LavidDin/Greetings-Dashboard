31756a9ff77fb0a08e751eefb85d5b67f682eae1
David Lin <LavidDin@users.noreply.github.com>
2018-11-25 10:59:56 -0500

    Add files via upload

---
+++ greetings-dashboard/main.js
@@ -0,0 +1,35 @@
+function openSettings() {
+    document.getElementById("settings").classList.toggle("settings-open");
+ }
+
+ document.getElementById("settings-button").addEventListener('click', openSettings)
+
+
+ var userName;
+
+ function saveName() {
+    localStorage.setItem('receivedName', userName);
+}
+var userName = localStorage.getItem('receivedName');
+
+
+if (userName == null) {
+    userName = "friend";
+ }
+
+ function changeName() {
+    userName = document.getElementById("name-input").value;
+    saveName();
+    getGreeting();
+ }
+
+ document.getElementById("name-form").addEventListener('submit', function(e) {
+    e.preventDefault()
+    changeName();
+ });
+
+ function getGreeting() {
+    document.getElementById("greeting").innerHTML  = `Greetings, ${userName}. Enjoy your day!`;
+ }
+ 
+ getGreeting()
